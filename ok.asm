cfa080ece96173e679a171370c47a4729162d467
TruncatedDinosour <71613062+TruncatedDinosour@users.noreply.github.com>
2022-03-30 18:59:11 +0000

    Create ok.asm

---
+++ ok.asm
@@ -0,0 +1,26 @@
+; mode: nasm-linux
+
+; $ nasm -felf64 ok.asm
+; $ ld -o ok ok.o
+; $ ./ok
+
+BITS 64
+
+global _start
+segment .text
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, sr
+    mov rdx, sl
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+segment .data
+
+sr: db "ok", 10
+sl: equ $-sr
