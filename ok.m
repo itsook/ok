c4f5fe5c8278ae7cd3889110f78b7a825db88afe
Harry <72156551+saimoomedits@users.noreply.github.com>
2022-04-01 16:04:13 +0400

    fix: actually print 'ok'

--- ok.m
+++ ok.m
@@ -3,7 +3,7 @@
 int main (int argc, const char * argv[])
 {
    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-   NSLog (@"Hello, World!");
+   NSLog (@"ok");
    [pool drain];    
    return 0; 
 } 
